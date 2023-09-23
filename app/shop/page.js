8eda65d9e0f3f5d17382dd77023f3b0669c50af4
AxLDvL <a.denis@it-students.fr>
2023-09-23 15:43:15 +0200

    feat(app): image tag on shop page

--- app/shop/page.js
+++ app/shop/page.js
@@ -1,25 +1,31 @@
-import styles from '../../styles/shop/shop.module.scss';
-import Navigation2 from '@components/Navigation2';
-import Footer from '@components/Footer';
+import styles from "../../styles/shop/shop.module.scss";
+import Navigation2 from "@components/Navigation2";
+import Footer from "@components/Footer";
+import Image from "next/image";
 
 export default function Shop() {
-    return (
-        <div>
-            <Navigation2 />
-                <div className={styles.page}>
-                    <div className={styles.container}>
-                        <img src="/images/WaterPilot.png" className={styles.image}></img>
-                        <div className={styles.details}>
-                            <h2 className={styles.title}>Water Pilot 2023</h2>
-                            <p className={styles.price}>$125</p>
-                            <p className={styles.description}>Le Water Pilot est le système d'arrosage automatique idéal pour votre jardin !</p>
-                            <button className={styles.button}>Ajouter au panier</button>
-                        </div>
-                    </div>
-                </div>
-            <Footer />
+  return (
+    <div>
+      <Navigation2 />
+      <div className={styles.page}>
+        <div className={styles.container}>
+          <Image
+            src="/images/WaterPilot.png"
+            className={styles.image}
+            alt="image du waterpilot"
+          />
+          <div className={styles.details}>
+            <h2 className={styles.title}>Water Pilot 2023</h2>
+            <p className={styles.price}>$125</p>
+            <p className={styles.description}>
+              Le Water Pilot est le système d'arrosage automatique idéal pour
+              votre jardin !
+            </p>
+            <button className={styles.button}>Ajouter au panier</button>
+          </div>
         </div>
-    )
+      </div>
+      <Footer />
+    </div>
+  );
 }
-
-
