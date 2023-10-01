991eb6bbc06a826a0cb48085383bc1eaad1249f1
AxLDvL <a.denis@it-students.fr>
2023-10-01 16:04:38 +0200

    feat(app): wip

--- app/shop/page.js
+++ app/shop/page.js
@@ -1,9 +1,36 @@
+"use client";
 import styles from "../../styles/shop/shop.module.scss";
 import Navigation2 from "@components/Navigation2";
 import Footer from "@components/Footer";
 import Image from "next/image";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 
 export default function Shop() {
+  const handlePurchase = async () => {
+    const stripe = await stripePromise;
+    console.log("stripe", stripe);
+    const response = await fetch("/api/charge", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        amount: 12500, // en centimes
+      }),
+    });
+
+    const session = await response.json();
+    console.log("session", session);
+    const result = await stripe.redirectToCheckout({
+      sessionId: session.sessionId,
+    });
+    if (result.error) {
+      alert(result.error.message);
+    }
+  };
+
   return (
     <div>
       <Navigation2 />
@@ -15,6 +42,7 @@ export default function Shop() {
             className={styles.image}
             width={500}
             height={500}
+            priority={true}
           />
           <div className={styles.details}>
             <h2 className={styles.title}>Water Pilot 2023</h2>
@@ -23,7 +51,9 @@ export default function Shop() {
               Le Water Pilot est le système d'arrosage automatique idéal pour
               votre jardin !
             </p>
-            <button className={styles.button}>Ajouter au panier</button>
+            <button className={styles.button} onClick={handlePurchase}>
+              Ajouter au panier
+            </button>
           </div>
         </div>
       </div>
