809c541c4efe48358a90a810383301e5cd12e46f
AxLDvL <a.denis@it-students.fr>
2023-10-07 17:20:40 +0200

    feat(app): adding toggle to schedules

--- components/dashboard/utils/Switch.js
+++ components/dashboard/utils/Switch.js
@@ -5,6 +5,9 @@ import { updateData } from "@utils/data-fetcher";
 export default function Switch({ endPoint, property, bool, id }) {
   const [toggle, setToggle] = useState(false);
   const containerClassName = `${styles.container} ${toggle ? styles.end : ""}`;
+  const containerClassNameSchedules = `${styles.containerschedules} ${
+    toggle ? styles.end : ""
+  }`;
 
   useEffect(() => {
     setToggle(bool ? true : false);
@@ -21,11 +24,26 @@ export default function Switch({ endPoint, property, bool, id }) {
     }
   };
 
-  return (
-    <div onClick={handleToggle} className={containerClassName}>
-      <motion.div className={styles.circle} layout>
-        <span className={styles.isAutoText}>{toggle ? "auto" : "manuel"}</span>
-      </motion.div>
-    </div>
-  );
+  switch (endPoint) {
+    case "electrovalves":
+      return (
+        <div onClick={handleToggle} className={containerClassName}>
+          <motion.div className={styles.circle} layout>
+            <span className={styles.isAutoText}>
+              {toggle ? "auto" : "manuel"}
+            </span>
+          </motion.div>
+        </div>
+      );
+    case "schedules":
+      return (
+        <div onClick={handleToggle} className={containerClassNameSchedules}>
+          <motion.div className={styles.circleschedules} layout></motion.div>
+        </div>
+      );
+    default:
+      return null; // ou return <> </>; si vous préférez
+  }
+
+  return null;
 }
